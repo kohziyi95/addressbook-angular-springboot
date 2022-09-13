b47225dcc9d2394aa99d48df47458a47538dd8e5
Zi Yi Koh <kohziyi95@gmail.com>
2022-09-13 17:42:29 +0800

    added CORS filter

--- addressbook-server/src/main/java/com/vttp/addressbookserver/configurations/AppConfig.java
+++ addressbook-server/src/main/java/com/vttp/addressbookserver/configurations/AppConfig.java
@@ -1,16 +1,11 @@
 package com.vttp.addressbookserver.configurations;
 
-import java.util.Collections;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -29,18 +24,4 @@ public class AppConfig {
         logger.info("CORS.Mapping: " + pathMapping + ". CORS.Origins: " + origins);
         return new CorsConfig(pathMapping, origins);
     }
-
-    // @Bean
-    // public FilterRegistrationBean corsFilter() {
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     CorsConfiguration config = new CorsConfiguration();
-    //     config.setAllowCredentials(true);
-    //     config.setAllowedOrigins(Collections.singletonList("*"));
-    //     config.addAllowedHeader("*");
-    //     config.addAllowedMethod("*");
-    //     source.registerCorsConfiguration("/**", config);
-    //     FilterRegistrationBean<CorsFilter> filterRegistration = new FilterRegistrationBean<>(new CorsFilter(source));
-    //     filterRegistration.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    //     return filterRegistration;
-    // }
 }
--- addressbook-server/src/main/java/com/vttp/addressbookserver/configurations/CorsFilter.java
+++ addressbook-server/src/main/java/com/vttp/addressbookserver/configurations/CorsFilter.java
@@ -1,37 +1,44 @@
-// package com.vttp.addressbookserver.configurations;
+package com.vttp.addressbookserver.configurations;
 
-// import java.io.IOException;
+import java.io.IOException;
 
-// import javax.servlet.Filter;
-// import javax.servlet.FilterChain;
-// import javax.servlet.FilterConfig;
-// import javax.servlet.ServletException;
-// import javax.servlet.ServletRequest;
-// import javax.servlet.ServletResponse;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
-// public class CorsFilter implements Filter {
+import org.springframework.core.annotation.Order;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
 
-//     @Override
-//     public void init(FilterConfig filterConfig) throws ServletException {
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
 
-//     }
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        final HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
 
-//     @Override
-//     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-//             throws IOException, ServletException {
-//         HttpServletResponse response = (HttpServletResponse) servletResponse;
-//         HttpServletRequest request = (HttpServletRequest) servletRequest;
+    @Override
+    public void destroy() {
+    }
 
-//         response.setHeader("Access-Control-Allow-Origin", "*");
-//         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
-//         response.setHeader("Access-Control-Allow-Headers", "*");
-//         filterChain.doFilter(servletRequest, servletResponse);
-//     }
-
-//     @Override
-//     public void destroy() {
-
-//     }
-// }
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+    }
+}
