6f3553377a9f6ce24449af37f0a5d24495e0985b
Zi Yi Koh <kohziyi95@gmail.com>
2022-09-13 17:08:51 +0800

    added CORS filter

--- addressbook-server/src/main/java/com/vttp/addressbookserver/configurations/AppConfig.java
+++ addressbook-server/src/main/java/com/vttp/addressbookserver/configurations/AppConfig.java
@@ -1,27 +1,46 @@
 package com.vttp.addressbookserver.configurations;
 
+import java.util.Collections;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration 
+@Configuration
 public class AppConfig {
 
     private Logger logger = Logger.getLogger(AppConfig.class.getName());
-    
+
     @Value("${cors.pathMapping}")
     String pathMapping;
 
     @Value("${cors.origins}")
     String origins;
 
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         logger.info("CORS.Mapping: " + pathMapping + ". CORS.Origins: " + origins);
         return new CorsConfig(pathMapping, origins);
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> filterRegistration = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterRegistration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return filterRegistration;
     }
 }
